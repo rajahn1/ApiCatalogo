4e4d0754f124888633cf263d10fbc56529390d56
Rafael <rafayuno@gmail.com>
2025-03-29 00:52:23 -0300

    upload files

--- ApiCatalogo/Infrastructure/Data/AppDbContext.cs
+++ ApiCatalogo/Infrastructure/Data/AppDbContext.cs
@@ -5,8 +5,10 @@ namespace ApiCatalogo.Infrastructure.Context;
 
 public class AppDbContext: DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base( options )
-    {}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 }
